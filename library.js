1f94e5901e03fdeff9cc71a76bc5cb1e36c4b6c2
Wilson Chu <wilson1.chu@gmail.com>
2022-02-08 12:24:35 -0500

    finished basic card UX, still need to clean CSS up

--- library.js
+++ library.js
@@ -50,7 +50,7 @@ class Book {
   constructor(title, author, pages, read) {
     this.title = form.title.value;
     this.author = form.author.value;
-    this.pages = form.pages.value + 'pg';
+    this.pages = form.pages.value + ' pg';
     this.read = form.read.checked;
   }
 }
